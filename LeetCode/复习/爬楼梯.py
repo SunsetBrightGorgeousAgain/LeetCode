35615fd5d8b7b8fe923ec54eafca655839045af7
Yujinneng <842617453@qq.com>
2021-03-29 11:13:02 +0800

    动态规划复习

---
+++ LeetCode/复习/爬楼梯.py
@@ -0,0 +1,50 @@
+"""
+70. 爬楼梯
+
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+注意：给定 n 是一个正整数。
+
+示例 1：
+
+输入： 2
+输出： 2
+解释： 有两种方法可以爬到楼顶。
+1.  1 阶 + 1 阶
+2.  2 阶
+
+示例 2：
+
+输入： 3
+输出： 3
+解释： 有三种方法可以爬到楼顶。
+1.  1 阶 + 1 阶 + 1 阶
+2.  1 阶 + 2 阶
+3.  2 阶 + 1 阶
+
+通过次数390,533
+提交次数756,730
+"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==1:
+            return 1
+        elif n==2:
+            return 2
+        else:
+            n1 = 1
+            n2 = 2
+            for i in range(3,n+1):
+                n3 = n1+n2
+                n1 = n2 
+                n2 = n3
+            return n3
+A=Solution()
+print(A.climbStairs(10))
+
+
+for i in range(3,10):
+    print(i)

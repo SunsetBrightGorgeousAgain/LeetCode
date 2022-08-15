dbb0b844483e9de81df9dbc0f2ff03166c829aeb
Yujinneng <842617453@qq.com>
2022-08-15 16:10:24 +0800

    19 感觉是使用了回溯,

---
+++ LeetCode/editor/leetcode/editor/cn/[18]四数之和.py
@@ -0,0 +1,5 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+# leetcode submit region end(Prohibit modification and deletion)
---
+++ LeetCode/editor/leetcode/editor/cn/[25]K 个一组翻转链表.py
@@ -0,0 +1,10 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+# leetcode submit region end(Prohibit modification and deletion)
